93386bb5055a7e8bae1639d89f6984991e3793fe
lpcarvalho97 <83229484+lpcarvalho97@users.noreply.github.com>
2021-05-16 14:23:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d"); /*renderiza o desenho dentro do canvas*/
+let box = 32;
+let snake = [];
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+}
+let direction = "right";
+let food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
+function criarBG(){
+    context.fillStyle = "yellow";
+    context.fillRect(0, 0, 16 * box, 16*box); /*desenha o retângulo onde ocorre o jogo. Trabalha com x,y, altura e largura*/
+}
+
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "red";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function drawFood (){
+    context.fillStyle = "purple";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+
+document.addEventListener('keydown', update);
+
+function update(event){  /*aqui são inseridas informações cartesianas, das quais a tutora já houvera descoberto para agilizar a tutoria, permitindo a movimentação da cobra no plano*/
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo(){
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
+    criarBG(); /*a retirada e a inclusão de elementos é o que faz a cobra movimentar-se*/
+    criarCobrinha();
+    drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y; /*ponto de partida da cobra*/
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
+
+    let newHead ={
+        x: snakeX, 
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); /*dá continuidade ao jogo sem que o mesmo trave a cada 100 milisegundos*/
+
